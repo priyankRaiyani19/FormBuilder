2c60b6403aefa280421fbc8506f6b95f8664528d
priyankRaiyani19 <priyankraiyani19@gmail.com>
2025-02-24 12:27:02 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import {BrowserRouter} from "react-router";
-import {DndProvider} from "react-dnd";
-import {HTML5Backend} from "react-dnd-html5-backend";
+// import {DndProvider} from "react-dnd";
+// import {HTML5Backend} from "react-dnd-html5-backend";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <DndProvider backend={HTML5Backend}>
+        {/*<DndProvider backend={HTML5Backend}>*/}
             <BrowserRouter>
                 <App/>
             </BrowserRouter>
-        </DndProvider>
+        {/*</DndProvider>*/}
     </React.StrictMode>
 );
